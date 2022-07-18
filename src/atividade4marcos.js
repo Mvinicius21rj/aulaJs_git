dc981ec6d6195695c5ac61543b164e5467821603
Marcos <mviniciusrj21@gmail.com>
2022-07-18 20:59:43 -0300

    primeiro

---
+++ src/atividade4marcos.js
@@ -0,0 +1,10 @@
+ /* 
+4  Solicite o preço de uma mercadoria e o percentual de desconto 20%.
+ Exiba no console o valor do desconto e o preço a pagar
+ */
+
+
+let rs = require ('readline-sync');
+
+let valor = rs.question('Digite o mês: ')
+
--- src/condicional_avancado_um.js
+++ src/condicional_avancado_um.js
@@ -4,3 +4,16 @@
   determine qual é o maior entre eles, se os números forem iguais, 
  mostre uma mensagem no console "Os números são iguais"
  */
+
+ 
+
+
+
+
+
+
+
+
+ /* 
+4  Solicite o proço de uma mercadoria e o percentual de desconto 20%.
+ */
