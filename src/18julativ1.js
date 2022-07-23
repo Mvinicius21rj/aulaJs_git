e99eca698a38db4967376555d6b0331a390739a5
1tjunior1 <cida_tadeu@yahoo.com.br>
2022-07-23 20:39:02 -0300

    Atividade 1

---
+++ src/18julativ1.js
@@ -0,0 +1,12 @@
+let rs= require('readline-sync');
+
+let numero1 = rs.questionInt("Digite o primeiro numero: ");
+let numero2 = rs.questionInt("Digite o segundo numero: ");
+
+if(numero1 === numero2){
+    console.log("Os números são iguais")
+}else if(numero1 > numero2){
+    console.log("O primeiro número é maior")    
+}else{
+    console.log("O segundo número é maior")
+}
